d25a963d7e76820d6c5da49ae9e00e033d26454b
jessicayeh <jessicayeh@berkeley.edu>
2018-02-22 15:29:49 -0800

    finished homework 2

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -1,24 +1,24 @@
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :nickname
 
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = name[0,4]
   end
 
   def nickname
-    # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @nickname
   end
 
   def birth_year
-    # YOUR IMPLEMENTATION HERE
-    1990
+    year = (Time.now.year).to_i
+    @age = (age).to_i
+    year - @age - 1
   end
 
   def introduction
-    # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    "I'm #{name} and I'm #{age} years old"
   end
 
 end
